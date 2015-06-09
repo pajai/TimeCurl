633010b3dcf9a7ef13f40f7f765460c9df87422d
pja <pja@zuehlke.com>
2015-06-09 21:28:46 +0200

    Added GPLv3 license

--- TimeWarp/ConfigureReportController.m
+++ TimeWarp/ConfigureReportController.m
@@ -1,10 +1,21 @@
-//
-//  ConfigureReportControllerViewController.m
-//  TimeWarp
-//
-//  Created by pat on 29.03.2014.
-//  Copyright (c) 2014 zuehlke. All rights reserved.
-//
+/*
+ 
+ Copyright (C) 2013-2015, Patrick Jayet
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+*/
 
 #import "ConfigureReportController.h"
 #import "SelectDayController.h"
